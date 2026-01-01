2f5f994303381d40578dc8bf53842754d809ddde
balch <balch61@gmail.com>
2026-01-01 09:32:34 -0800

    docs: Add KDoc documentation to runCatchingSuspend
    - Explain the purpose of the extension function for encapsulating suspend operations in `Result`.
    - Clarify the behavior regarding `CancellationException` propagation for proper coroutine cancellation support.
    - Document parameters and return values.

--- composeApp/src/commonMain/kotlin/org/balch/orpheus/core/coroutines/RunCatchingSuspend.kt
+++ composeApp/src/commonMain/kotlin/org/balch/orpheus/core/coroutines/RunCatchingSuspend.kt
@@ -2,6 +2,21 @@ package org.balch.orpheus.core.coroutines
 
 import kotlin.coroutines.cancellation.CancellationException
 
+/**
+ * Executes the given suspending block in the context of the receiver and returns a [Result] that
+ * encapsulates the success or failure of the operation.
+ *
+ * This convenience method allows propagating [CancellationException]
+ * up the call stack to adhere to Android best practices for
+ * suspend functions.
+ *
+ * If the block completes successfully, its result is wrapped in a [Result.success].
+ * If an exception occurs, it is caught and wrapped in a [Result.failure]. Note that
+ * [CancellationException] is rethrown to respect coroutine cancellation.
+ *
+ * @param block The suspending block of code to execute.
+ * @return A [Result] wrapping the outcome of the operation.
+ */
 suspend inline fun <T, R> T.runCatchingSuspend(
     block: suspend T.() -> R,
 ) = try {
